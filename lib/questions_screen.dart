dce9be89e7b7839e6529ac7f4650efc73c92be4c
Dowon Kim <dowoniscool@yahoo.com>
2024-01-17 10:24:24 +0100

    Style: Answer buttons take up all the available space, centering the texts

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -16,20 +16,24 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(currentQuestion.question,
-              style: const TextStyle(color: Colors.white)),
-          const SizedBox(height: 30),
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(
-                answerText: answer,
-                selectAnswer: () {
-                  print('Answer selected');
-                });
-          }),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch, // (Buttons )Stretch to full width of screen
+          children: [
+            Text(currentQuestion.question,
+                style: const TextStyle(color: Colors.white),textAlign: TextAlign.center,),
+            const SizedBox(height: 30),
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(
+                  answerText: answer,
+                  selectAnswer: () {
+                    print('Answer selected');
+                  });
+            }),
+          ],
+        ),
       ),
     );
   }
