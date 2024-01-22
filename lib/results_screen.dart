fec687bac47306a506d88f1674ddeb55034fdf42
Dowon Kim <dowoniscool@yahoo.com>
2024-01-22 21:45:38 +0100

    Added result screen

---
+++ lib/results_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ResultScreen extends StatelessWidget {
+  const ResultScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment
+              .stretch, // (Buttons )Stretch to full width of screen
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
