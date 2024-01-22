14daf974ac5a9a5d6b18197ee8065ef0a21a06a7
Dowon Kim <dowoniscool@yahoo.com>
2024-01-22 21:46:33 +0100

    Added Children elements

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -14,7 +14,30 @@ class ResultScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment
               .stretch, // (Buttons )Stretch to full width of screen
           children: [
-
+            const Text(
+              'You have completed the quiz!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color.fromRGBO(176, 98, 255, 1),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                backgroundColor: const Color.fromARGB(255, 96, 66, 227),
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40),
+                ),
+              ),
+              child: const Text('Restart Quiz'),
+            ),
           ],
         ),
       ),
