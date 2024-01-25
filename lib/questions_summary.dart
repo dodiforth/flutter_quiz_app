34cdbcfeab6a2fd3664f19e87910daa2ba166195
Dowon Kim <dowoniscool@yahoo.com>
2024-01-25 18:23:11 +0100

    Style : Make result summary index colored

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -9,17 +9,37 @@ class QuestionSummary extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 300,
-      child: SingleChildScrollView (
+      child: SingleChildScrollView(
         child: Column(
           children: summaryData.map((data) {
+            bool isCorrect = data['userAnswer'] == data['correctAnswer'];
             return Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(((data['question_index'] as int) + 1).toString()),
-                Expanded (
+                Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  width: 30,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: isCorrect
+                        ? const Color.fromARGB(255, 10, 121, 211)
+                        : const Color.fromARGB(255, 227, 85, 229),
+                  ),
+                  child: Center(
+                    child: Text(
+                      ((data['question_index'] as int) + 1).toString(),
+                      style: const TextStyle(
+                        color: Color.fromARGB(255, 51, 19, 57),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
                   child: Column(
                     children: [
                       Text(data['question'] as String),
-                      SizedBox(height: 10),
                       Text(data['userAnswer'] as String),
                       Text(data['correctAnswer'] as String),
                     ],
