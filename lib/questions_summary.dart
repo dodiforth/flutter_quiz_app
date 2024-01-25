90f08d4e5c7989b2c53b2ee1d0075e601732a2c5
Dowon Kim <dowoniscool@yahoo.com>
2024-01-25 19:13:12 +0100

    Style : Finish question_summary section design

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionSummary extends StatelessWidget {
   const QuestionSummary({super.key, required this.summaryData});
@@ -13,39 +14,55 @@ class QuestionSummary extends StatelessWidget {
         child: Column(
           children: summaryData.map((data) {
             bool isCorrect = data['userAnswer'] == data['correctAnswer'];
-            return Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(right: 20),
-                  width: 30,
-                  height: 30,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: isCorrect
-                        ? const Color.fromARGB(255, 10, 121, 211)
-                        : const Color.fromARGB(255, 227, 85, 229),
-                  ),
-                  child: Center(
-                    child: Text(
-                      ((data['question_index'] as int) + 1).toString(),
-                      style: const TextStyle(
-                        color: Color.fromARGB(255, 51, 19, 57),
-                        fontWeight: FontWeight.bold,
+            return Container(
+              margin: const EdgeInsets.only(bottom: 5),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(right: 20),
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: isCorrect
+                          ? const Color.fromARGB(255, 10, 121, 211)
+                          : const Color.fromARGB(255, 227, 85, 229),
+                    ),
+                    child: Center(
+                      child: Text(
+                        ((data['question_index'] as int) + 1).toString(),
+                        style: const TextStyle(
+                          color: Color.fromARGB(255, 51, 19, 57),
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Column(
-                    children: [
-                      Text(data['question'] as String),
-                      Text(data['userAnswer'] as String),
-                      Text(data['correctAnswer'] as String),
-                    ],
-                  ),
-                )
-              ],
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          data['question'] as String,
+                          style: GoogleFonts.montserrat(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          data['userAnswer'] as String,
+                          style: GoogleFonts.lato(
+                              color: const Color.fromRGBO(255, 255, 255, 0.5)),
+                        ),
+                        Text(
+                          data['correctAnswer'] as String,
+                          style: GoogleFonts.lato(
+                              color: const Color.fromARGB(255, 69, 166, 246)),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             );
           }).toList(),
         ),
